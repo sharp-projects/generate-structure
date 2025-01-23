983a313189cb0eb7d722343e82246039a2afec56
Kevin Sharp <sharp.projects.101@gmail.com>
2025-01-23 09:56:50 -0700

    Second round of exercises

---
+++ generate_structure.go
@@ -0,0 +1,17 @@
+package main
+
+// list of directories
+
+// list of files
+
+// function to create the directories
+
+// function to create the files
+
+// function to validate that all directories and files were created
+
+func main() {
+	// create directories
+	// create files
+	// validate the structure
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sharp-projects/GrowthEngine/GoLang/generate-structure
+
+go 1.21.5
