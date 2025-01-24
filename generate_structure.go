cd8b68dadd94c81269229904a9c2bae2e12cc094
Kevin Sharp <sharp.projects.101@gmail.com>
2025-01-24 13:24:36 -0700

    Third round of exercises and skeleton of generate structure

--- generate_structure.go
+++ generate_structure.go
@@ -1,16 +1,56 @@
 package main
 
+import "fmt"
+
 // list of directories
+var directories = []string{
+	"cmd",
+	"cmd/app",
+	"configs",
+	"internal/handlers",
+	"internal/services",
+	"internal/repositories",
+	"internal/models",
+	"pkg/logger",
+	"pkg/config",
+	"pkg/errors",
+	"migrations",
+	"tests/integration",
+	"tests/unit",
+	"tests/mocks",
+	"docs",
+}
 
 // list of files
+var files = []string{
+	"README.md",
+	"LICENSE",
+	".gitignore",
+	"Makefile",
+	"Dockerfile",
+	"docker-compose.yml",
+	".env",
+}
 
 // function to create the directories
+func createDirectories() {
+	fmt.Println("Creating directories")
+}
 
 // function to create the files
+func createFiles() {
+	fmt.Println("Creating files")
+}
 
 // function to validate that all directories and files were created
+func validateStructure() {
+	fmt.Println("Validating structure")
+}
 
 func main() {
+	createDirectories()
+	createFiles()
+	validateStructure()
 	// create directories
 	// create files
 	// validate the structure
